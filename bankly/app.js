bbe079fad5381aeb60b8b092bcad6886d1693c65
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-26 14:33:41 -0400

    Add Express app export in app.js

--- bankly/app.js
+++ bankly/app.js
@@ -1,35 +1,33 @@
-/** Application for bank.ly */
-
 const express = require("express");
 const app = express();
 const ExpressError = require("./helpers/expressError");
-const { authenticateJWT } = require("./middleware/auth");
-
+const { authUser } = require("./middleware/auth");
 
 app.use(express.json());
+
+// Apply authUser middleware globally before any route definitions
+app.use(authUser);
 
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
 app.use('/users', userRoutes);
-
-app.use(authenticateJWT);
 app.use('/auth', authRoutes);
 
-/** 404 handler */
+// Add the new route handler for the root URL here
+app.get('/', (req, res) => {
+  res.send('Welcome to the Bankly App!');
+});
 
+/** 404 handler */
 app.use(function(req, res, next) {
   const err = new ExpressError("Not Found", 404);
-
-  // pass the error to the next piece of middleware
   return next(err);
 });
 
 /** general error handler */
-
 function errorHandler(err, req, res, next) {
   let status = err.status || 500;
   let message = err.message;
-
   return res.status(status).json({
     error: { message, status }
   });
@@ -37,8 +35,4 @@ function errorHandler(err, req, res, next) {
 
 app.use(errorHandler);
 
-module.exports = {
-  app,
-  errorHandler
-};
-
+module.exports = app;
