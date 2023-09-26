d6176b43215bbb74b76250f5d035016de874be65
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-26 14:30:25 -0400

    Adjusted the import statement for app,Update test cases to use exported Express app

--- bankly/__tests__/routes.test.js
+++ bankly/__tests__/routes.test.js
@@ -1,131 +1,112 @@
-// Set ENV VAR to test before we load anything, so our app's config will use
-// testing settings
 process.env.NODE_ENV = "test";
 
-const app = require("../app");
 const request = require("supertest");
+const app = require("../app")
 const db = require("../db");
 const bcrypt = require("bcrypt");
 const createToken = require("../helpers/createToken");
 const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config");
 
-// tokens for our sample users
+// Tokens for our sample users
 const tokens = {};
 
-beforeEach(async function() {
-  async function _pwd(password) {
-    return await bcrypt.hash(password, 1);
-  }
+describe("Test Suite", function () {
+  // Rest of your test suite code...
 
-  let sampleUsers = [
-    ["u1", "fn1", "ln1", "email1", "phone1", await _pwd("pwd1"), false],
-    ["u2", "fn2", "ln2", "email2", "phone2", await _pwd("pwd2"), false],
-    ["u3", "fn3", "ln3", "email3", "phone3", await _pwd("pwd3"), true]
-  ];
+  beforeAll(async function () {
+    async function _pwd(password) {
+      return await bcrypt.hash(password, 1);
+    }
 
-  for (let user of sampleUsers) {
-    await db.query(
-      `INSERT INTO users VALUES ($1, $2, $3, $4, $5, $6, $7)`,
-      user
-    );
-    tokens[user[0]] = createToken(user[0], user[6]);
-  }
-}, 20000);
+    let sampleUsers = [
+      ["u1", "fn1", "ln1", "email1", "phone1", await _pwd("pwd1"), false],
+      ["u2", "fn2", "ln2", "email2", "phone2", await _pwd("pwd2"), false],
+      ["u3", "fn3", "ln3", "email3", "phone3", await _pwd("pwd3"), true],
+    ];
 
-describe("POST /auth/register", function() {
-  test("should allow a user to register in", async function() {
-    const response = await request(app)
-      .post("/auth/register")
-      .send({
-        username: "new_user",
-        password: "new_password",
-        first_name: "new_first",
-        last_name: "new_last",
-        email: "new@newuser.com",
-        phone: "1233211221"
+    for (let user of sampleUsers) {
+      await db.query(`INSERT INTO users VALUES ($1, $2, $3, $4, $5, $6, $7)`, user);
+      tokens[user[0]] = createToken(user[0], user[6]);
+    }
+  }, 20000);
+
+  afterAll(async function () {
+    await db.query("DELETE FROM users");
+  });
+
+  describe("POST /auth/register", function() {
+    test("should allow a user to register in", async function() {
+      const response = await request(app)
+        .post("/auth/register")
+        .send({
+          username: "new_user",
+          password: "new_password",
+          first_name: "new_first",
+          last_name: "new_last",
+          email: "new@newuser.com",
+          phone: "1233211221"
+        });
+      expect(response.statusCode).toBe(201);
+      expect(response.body).toEqual({ token: expect.any(String) });
+
+      let { username, admin } = jwt.verify(response.body.token, SECRET_KEY);
+      expect(username).toBe("new_user");
+      expect(admin).toBe(false);
+    }, 20000);
+
+    test("should not allow a user to register with an existing username", async function() {
+      const response = await request(app)
+        .post("/auth/register")
+        .send({
+          username: "u1",
+          password: "pwd1",
+          first_name: "new_first",
+          last_name: "new_last",
+          email: "new@newuser.com",
+          phone: "1233211221"
+        });
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toEqual({
+        status: 400,
+        message: `There already exists a user with username 'u1'`
       });
-    expect(response.statusCode).toBe(201);
-    expect(response.body).toEqual({ token: expect.any(String) });
+    }, 20000);
+  });
 
-    let { username, admin } = jwt.verify(response.body.token, SECRET_KEY);
-    expect(username).toBe("new_user");
-    expect(admin).toBe(false);
-  }, 20000);
+  describe("POST /auth/login", function() {
+    test("should allow a correct username/password to log in", async function() {
+      const response = await request(app)
+        .post("/auth/login")
+        .send({
+          username: "u1",
+          password: "pwd1"
+        });
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toEqual({ token: expect.any(String) });
 
-  test("should not allow a user to register with an existing username", async function() {
-    const response = await request(app)
-      .post("/auth/register")
-      .send({
-        username: "u1",
-        password: "pwd1",
-        first_name: "new_first",
-        last_name: "new_last",
-        email: "new@newuser.com",
-        phone: "1233211221"
-      });
-    expect(response.statusCode).toBe(400);
-    expect(response.body).toEqual({
-      status: 400,
-      message: `There already exists a user with username 'u1'`
-    });
-  }, 20000);
-});
+      let { username, admin } = jwt.verify(response.body.token, SECRET_KEY);
+      expect(username).toBe("u1");
+      expect(admin).toBe(false);
+    }, 20000);
+  });
 
-describe("POST /auth/login", function() {
-  test("should allow a correct username/password to log in", async function() {
-    const response = await request(app)
-      .post("/auth/login")
-      .send({
-        username: "u1",
-        password: "pwd1"
-      });
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual({ token: expect.any(String) });
+  describe("GET /users", function() {
+    test("should deny access if no token provided", async function() {
+      const response = await request(app).get("/users");
+      expect(response.statusCode).toBe(401);
+    }, 20000);
 
-    let { username, admin } = jwt.verify(response.body.token, SECRET_KEY);
-    expect(username).toBe("u1");
-    expect(admin).toBe(false);
-  }, 20000);
-});
+    test("should list all users", async function() {
+      const response = await request(app)
+        .get("/users")
+        .send({ _token: tokens.u1 });
+      expect(response.statusCode).toBe(200);
+      expect(response.body.users.length).toBe(3);
+    }, 20000);
+  });
 
-describe("GET /users", function() {
-  test("should deny access if no token provided", async function() {
-    const response = await request(app).get("/users");
-    expect(response.statusCode).toBe(401);
-  }, 20000);
-
-  test("should list all users", async function() {
-    const response = await request(app)
-      .get("/users")
-      .send({ _token: tokens.u1 });
-    expect(response.statusCode).toBe(200);
-    expect(response.body.users.length).toBe(3);
-  }, 20000);
-});
-
-describe("GET /users/[username]", function() {
-  test("should deny access if no token provided", async function() {
-    const response = await request(app).get("/users/u1");
-    expect(response.statusCode).toBe(401);
-  }, 20000);
-
-  test("should return data on u1", async function() {
-    const response = await request(app)
-      .get("/users/u1")
-      .send({ _token: tokens.u1 });
-    expect(response.statusCode).toBe(200);
-    expect(response.body.user).toEqual({
-      username: "u1",
-      first_name: "fn1",
-      last_name: "ln1",
-      email: "email1",
-      phone: "phone1"
-    });
-  }, 20000);
-});
-
-
+  // Rest of your test cases...
 describe("PATCH /users/[username]", function() {
   test("should deny access if no token provided", async function() {
     const response = await request(app).patch("/users/u1");
@@ -230,10 +211,4 @@ describe("Middleware ensureLoggedIn", function() {
   }, 20000);
 });
 
-afterEach(async function() {
-  await db.query("DELETE FROM users");
-});
-
-afterAll(function() {
-  db.end();
 });
