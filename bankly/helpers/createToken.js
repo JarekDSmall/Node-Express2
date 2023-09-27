e40f36b76f58212d22af5790501c299fb069221c
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-26 20:28:04 -0400

    Update createToken.js

--- bankly/helpers/createToken.js
+++ bankly/helpers/createToken.js
@@ -2,6 +2,16 @@ const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config");
 
 function createToken(user) {
+  if (!user) {
+    throw new Error("Invalid user object: User object is undefined or null");
+  }
+  if (!user.username) {
+    throw new Error("Invalid user object: Username is undefined");
+  }
+  if (typeof user.is_admin === 'undefined') {
+    throw new Error("Invalid user object: is_admin property is undefined");
+  }
+
   let payload = {
     username: user.username,
     is_admin: user.is_admin,
