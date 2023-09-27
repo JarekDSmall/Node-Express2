2c7f41830f554fb4568205634db4a125eb422403
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-26 20:26:45 -0400

    removed requireLogin for redundancy.

--- bankly/middleware/auth.js
+++ bankly/middleware/auth.js
@@ -1,57 +1,67 @@
 const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config");
+const ExpressError = require("../expressError");
 
 /** Middleware: Authenticate user. */
 function authUser(req, res, next) {
   try {
-    const tokenFromBody = req.body._token;
-    const payload = jwt.verify(tokenFromBody, SECRET_KEY);
-    req.user = payload; // create a current user
+
+    console.log('Authorization Header:', req.headers.authorization);
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      throw new ExpressError("Unauthorized: Token missing", 401);
+    }
+
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      throw new ExpressError("Unauthorized: Token malformed", 401);
+    }
+
+// Check if the token has three parts separated by dots
+    const tokenParts = token.split(".");
+    if (tokenParts.length !== 3) {
+      throw new ExpressError("Unauthorized: Token malformed", 401);
+    }
+
+    // Log the received token
+    console.log('Received Token:', token);
+
+    const payload = jwt.verify(token, SECRET_KEY);
+    req.user = payload;
+
     return next();
   } catch (err) {
-    return next();
-  }
-}
-
-/** Middleware: Requires user is authenticated. */
-function requireLogin(req, res, next) {
-  if (!req.user) {
-    return res.status(401).json({ message: "Unauthorized" });
-  } else {
-    return next();
+    return next(err);
   }
 }
 
 /** Middleware: Requires correct username. */
 function ensureCorrectUser(req, res, next) {
   try {
-    if (req.user.username === req.params.username) {
-      return next();
-    } else {
-      return res.status(401).json({ message: "Unauthorized" });
+    if (!req.user || req.user.username !== req.params.username) {
+      throw new ExpressError("Unauthorized: Incorrect username", 401);
     }
+    return next();
   } catch (err) {
-    // errors would happen here if we made a request and req.user is undefined
-    return res.status(401).json({ message: "Unauthorized" });
+    return next(err);
   }
 }
 
-/** Middleware: Requires user is an admin. */
+/** Middleware: Requires user to be an admin. */
 function requireAdmin(req, res, next) {
   try {
-    if (req.user && req.user.is_admin) {
-      return next();
-    } else {
-      return res.status(403).json({ message: "Forbidden: Admin privileges required" });
+    if (!req.user || !req.user.is_admin) {
+      throw new ExpressError("Forbidden: Admin privileges required", 403);
     }
+    return next();
   } catch (err) {
-    return res.status(403).json({ message: "Forbidden: Admin privileges required" });
+    return next(err);
   }
 }
 
 module.exports = {
   authUser,
-  requireLogin,
   ensureCorrectUser,
-  requireAdmin
+  requireAdmin,
 };
